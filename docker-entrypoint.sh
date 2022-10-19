643d7c47c752a9f2a8c9aaa2608d5b0a18d9425e
llussy <llussy.me@gmail.com>
2022-10-19 15:12:17 +0800

    update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# https://stackoverflow.com/questions/53874301/how-to-run-command-when-container-is-started-docker
 nginx && sleep 0.5s && /usr/local/bin/pm2 start /ecosystem.config.js & /usr/local/bin/pm2 log 
+
