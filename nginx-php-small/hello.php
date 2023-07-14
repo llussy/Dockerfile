7748aeb7368dde1a74e4adc17d86e86e30099994
llussy <llussy.me@gmail.com>
2023-07-14 17:37:10 +0800

    add python

--- nginx-php-small/hello.php
+++
@@ -1,8 +0,0 @@
-<html>
- <head>
-  <title>PHP 测试</title>
- </head>
- <body>
- <p>Hello World</p>
- </body>
-</html>
--- nginx-php-small/test.php
+++
@@ -1,3 +0,0 @@
-<?php
-  file_put_contents("php://stdout", "message sent by stdout" . PHP_EOL);
-?>
